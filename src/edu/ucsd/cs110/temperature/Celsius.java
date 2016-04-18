52f304949cf70253efec17145da52cfa1904d296
Joseph Widjaja <jewidjaj@ucsd.edu>
2016-04-18 11:34:10 -0700

    debug

--- src/edu/ucsd/cs110/temperature/Celsius.java
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -12,18 +12,19 @@ public class Celsius extends Temperature {
 
     @Override
     public Temperature toCelsius() {
-        return null;
+        return this;
     }
 
     @Override
     public Temperature toFahrenheit() {
-        return null;
+        float fahrenheit = 9*this.value/5 + 32;
+        return new Fahrenheit(fahrenheit);
     }
 
 
     public String toString()
         {
             // TODO: Complete this method
-            return "";
+            return this.value+" C";
         }
 }
--- src/edu/ucsd/cs110/temperature/Fahrenheit.java
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -11,19 +11,20 @@ public class Fahrenheit extends Temperature {
 
     @Override
     public Temperature toCelsius() {
-        return null;
+        float celsius = ((this.value-32)*5)/9;
+        return new Celsius(celsius);
     }
 
     @Override
     public Temperature toFahrenheit() {
-        return null;
+        return this;
     }
 
 
     public String toString()
     {
         // TODO: Complete this method
-        return "";
+        return this.value+" F";
     }
 }
 
