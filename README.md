640a67a3eab3e9e848262348a64431beaac618fc
Joseph Widjaja <jewidjaj@ucsd.edu>
2016-04-18 11:18:23 -0700

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](BUILD_STATUS_IMAGE)](BUILD_STATUS_LINK)
+# Travis CI Project with Junit
+----------
+The module above will show whether your most recent Travis CI 
+build has passed or failed the Junit tests. 
--- src/edu/ucsd/cs110/temperature/CelsiusTest.java
+++ src/edu/ucsd/cs110/temperature/CelsiusTest.java
@@ -1,23 +1,21 @@
 package edu.ucsd.cs110.temperature;
 import static org.junit.Assert.*;
 import org.junit.Test;
-public class FahrenheitTest {
+public class CelsiusTest {
     private float delta = 0.001f;
-
     @Test
-    public void testFahrenheit() {
+    public void testCelsius(){
         float value = 12.34f;
-        Fahrenheit temp = new Fahrenheit(value);
+        Celsius temp = new Celsius(value);
         assertEquals(value, temp.getValue(), delta);
     }
-
     @Test
-    public void testFahrenheitToString() {
+    public void testCelsiusToString(){
         float value = 12.34f;
-        Fahrenheit temp = new Fahrenheit(value);
+        Celsius temp = new Celsius(value);
         String string = temp.toString();
         String beginning = "" + value;
-        String ending = " F";
+        String ending = " C";
 // Verify the suffix of the formatted string
         assertTrue(string.startsWith(beginning));
 // Verify the prefix of the formatted string
@@ -27,21 +25,20 @@ public class FahrenheitTest {
 // (Hint: what is the length of the middle of the string?)
         assertTrue(string.substring(0, endIndex).equals(beginning));
     }
-
     @Test
-    public void testFahrenheitToFahrenheit() {
-        Fahrenheit temp = new Fahrenheit(32);
-        Temperature convert = temp.toFahrenheit();
-        assertEquals(32, convert.getValue(), delta);
-    }
-
-    @Test
-    public void testFahrenheitToCelsius() {
-        Fahrenheit temp = new Fahrenheit(32);
+    public void testCelsiusToCelsius()
+    {
+        Celsius temp = new Celsius(0);
         Temperature convert = temp.toCelsius();
         assertEquals(0, convert.getValue(), delta);
-        temp = new Fahrenheit(212);
-        convert = temp.toCelsius();
-        assertEquals(100, convert.getValue(), delta);
+    }
+    @Test
+    public void testCelsiusToFahrenheit(){
+        Celsius temp = new Celsius(0);
+        Temperature convert = temp.toFahrenheit();
+        assertEquals(32, convert.getValue(), delta);
+        temp = new Celsius(100);
+        convert = temp.toFahrenheit();
+        assertEquals(212, convert.getValue(), delta);
     }
 }
